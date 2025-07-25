312e699a04430588ff7949d23a476208e112e6b7
Alexgv0 <AlexGamerVideos@hotmail.com>
2025-07-25 19:28:40 -0300

    server separated

--- Servidor/routes/iaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const iaController = require('../controllers/iaController.js');
-
-// Ruta POST para la IA
-router.post('/', iaController.getResponse);
-
-module.exports = router;
